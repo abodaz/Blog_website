5c887cf1937698904286ee6095f41f6381dd6f9e
unknown <abodaz100@gmail.com>
2020-01-21 01:44:31 +0200

    post modle for database

--- django_project/blogs/views.py
+++ django_project/blogs/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-posts = [
-    {
-        'author': 'Abdullah',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'data_posted': 'August 27, 2018',
-
-    },
-    {
-        'author': 'Mohammed',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'data_posted': 'August 28, 2018',
-
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
